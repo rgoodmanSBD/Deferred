0529665c9755343f7b4fa4e555a0c48e8bb0858e
Zachary Waldowski <zach@waldowski.me>
2015-10-28 22:41:47 -0400

    [Task] Extensions to initialize futures and tasks using functions

---
+++ Sources/Task/CrossPromises.swift
@@ -0,0 +1,44 @@
+//
+//  CrossFutures.swift
+//  Deferred
+//
+//  Created by Zachary Waldowski on 10/27/15.
+//  Copyright © 2014-2015 Big Nerd Ranch. Licensed under MIT.
+//
+
+#if SWIFT_PACKAGE
+import Deferred
+import Result
+#endif
+import Dispatch
+
+extension PromiseType {
+
+    init<Other: FutureType where Other.Value == Value>(_ other: Other) {
+        self.init()
+        other.upon {
+            self.fill($0)
+        }
+    }
+
+    init(@noescape with body: () -> Value) {
+        self.init()
+        fill(body())
+    }
+
+}
+
+extension PromiseType where Value: ResultType {
+
+    init(@noescape with body: () throws -> Value.Value) {
+        self.init()
+        let value: Value
+        do {
+            value = Value(value: try body())
+        } catch {
+            value = Value(error: error)
+        }
+        fill(value)
+    }
+
+}
